f19060d17852fb93a301edaa450d1f5a7fd02b0a
Joel Scoble <joel.scoble@outlook.com>
2015-08-25 23:15:40 -0500

    update FormatType stuff to eliminate collision; update Resource struct and add some methods

--- mog.go
+++ mog.go
@@ -25,14 +25,14 @@ type mogger interface{} {
 }
 */
 const (
-	UnsupportedFormat FormatType = iota
-	CSV
-	MD
-	MDTable
+	FmtUnsupported FormatType = iota
+	FmtCSV
+	FmtMD
+	FmtMDTable
 )
 
-const(
-	UnsupportedResource ResourceType  = iota
+const (
+	UnsupportedResource ResourceType = iota
 	File
 )
 
@@ -45,20 +45,20 @@ var formatTypes = [...]string{
 	"unsupported",
 	"csv",
 	"md",
-	"mdtable"
+	"mdtable",
 }
 
 func FormatTypeFromString(s string) FormatType {
 	s = strings.ToLower(s)
 	switch s {
 	case "csv":
-		return CSV
+		return FmtCSV
 	case "md":
-		return MD
+		return FmtMD
 	case "mdtable":
-		return MDTable
+		return FmtMDTable
 	}
-	return UnsupportedFormat
+	return FmtUnsupported
 }
 
 // ResourceType is the type of a resource
@@ -68,7 +68,7 @@ func (r ResourceType) String() string { return resourceTypes[r] }
 
 var resourceTypes = [...]string{
 	"unsupported",
-	"file"
+	"file",
 }
 
 // ResourceTypeFromString returns the ResourceType constant
@@ -89,25 +89,27 @@ var (
 // Currently only supporting local file.
 // TODO enable uri support
 type resource struct {
-	Name string         // Name of the resource
-	Path string         // Path of the resource
+	Name   string       // Name of the resource
+	Path   string       // Path of the resource
 	Host   string       // Host of the resource
-	Format string       // Format of the resource
+	Format FormatType   // Format of the resource
 	Type   ResourceType // Type of the resource
 }
 
-func NewResource(s string, t ResourceType) resource {
+func NewResource(s string, f FormatType, t ResourceType) resource {
 	if s == "" {
-		return resource{Type: t}
+		return resource{Format: f, Type: t}
 	}
 	dir := path.Dir(s)
 	// if the path didn't contain a directory, make dir an empty string
 	if dir == "." {
 		dir = ""
 	}
-	return resource{Name: path.Base(s), Path: dir, Type: t}
+	return resource{Name: path.Base(s), Path: dir, Format: f, Type: t}
 }
 
+// String() returns the resource as a string. The value depends on the format
+// and type.
 func (r resource) String() string {
 	if r.Path == "" {
 		return r.Name
@@ -115,10 +117,26 @@ func (r resource) String() string {
 	return filepath.Join(r.Path, r.Name)
 }
 
+// SetName sets the resource name.
 func (r *resource) SetName(s string) {
 	r.Name = s
 }
 
+// SetPath sets the resource path.
 func (r *resource) SetPath(s string) {
 	r.Path = s
+}
+
+// SetFormat takes the passed string and sets the Format. If the string is not
+// a supported Format, UnsupportedFormat will be used. It may be useful to
+// check the value of the Format after setting.
+func (r *resource) SetFormat(s string) {
+	r.Format = FormatFromString(s)
+}
+
+// SetResourceType takes the passed string and sets the Type. If the string is
+// not a supported Resource type, UnsupportedResource will be used. It may be
+// useful to check the value of the Type after setting.
+func (r *resource) SetResourceType(s string) {
+	r.Type = ResourceTypeFromString(s)
 }
--- mog_test.go
+++ mog_test.go
@@ -5,15 +5,17 @@ import "testing"
 func TestNewResource(t *testing.T) {
 	tests := []struct {
 		path     string
+		format   FormatType
+		typ      ResourceType
 		expected resource
 	}{
-		{"", resource{}},
-		{"test.txt", resource{Name: "test.txt"}},
-		{"path/test.txt", resource{Name: "test.txt", Path: "path"}},
-		{"another/path/test", resource{Name: "test", Path: "another/path"}},
+		{"", FmtUnsupported, UnsupportedResource, resource{Format: FmtUnsupported, Type: UnsupportedResource}},
+		{"test.txt", FmtUnsupported, File, resource{Name: "test.txt", Format: FmtUnsupported, Type: UnsupportedResource}},
+		{"path/test.csv", FmtCSV, File, resource{Name: "test.txt", Path: "path", Format: FmtCSV, Type: File}},
+		{"another/path/test", FmtMDTable, File, resource{Name: "test", Path: "another/path", Format: FmtMDTable, Type: File}},
 	}
 	for _, test := range tests {
-		r := NewResource(test.path)
+		r := NewResource(test.path, test.format, test.typ)
 		if r != test.expected {
 			t.Errorf("Expected %+v, got %+v", test.expected, r)
 		}
